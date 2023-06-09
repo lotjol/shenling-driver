5ddf42314b5d7f7aae8ccde56e7de700aa32e3d3
lotjol <botue@sina.com>
2023-06-09 14:42:31 +0800

    chore(taskdata): remove some logic

--- subpkg_user/task/index.vue
+++ subpkg_user/task/index.vue
@@ -1,17 +1,10 @@
 <script setup>
-  import { reactive } from 'vue'
   import slUniCalendar from './components/sl-uni-calendar/sl-uni-calendar'
-
-  const selected = reactive([
-    { date: '2023-03-24', info: '332 ' },
-    { date: '2023-03-25', info: '249' },
-    { date: '2023-03-26', info: '312' },
-  ])
 </script>
 
 <template>
   <view class="page-container">
-    <sl-uni-calendar :selected="selected" :show-month="false">
+    <sl-uni-calendar :show-month="false">
       <view class="data-overview">
         <view class="item">
           <text class="volumn">18</text>
