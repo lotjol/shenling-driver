9ef4c865aa7b8a1abe6deaa4ed34c0e43c3d747f
lotjol <botue@sina.com>
2023-06-14 14:46:42 +0800

    chore(record): some css about record

--- subpkg_task/record/index.vue
+++ subpkg_task/record/index.vue
@@ -9,19 +9,32 @@
 </script>
 <template>
   <view class="page-container">
-    <uni-list class="base-info">
-      <uni-list-item title="出车时间" show-arrow right-text="2022-05-04 13:00:00" />
-      <uni-list-item show-arrow title="回车时间">
-        <template v-slot:footer>
-          <uni-datetime-picker v-model="dataTimePicker">
-            <view class="picker-value">{{ dataTimePicker || '请选择' }}</view>
-          </uni-datetime-picker>
-        </template>
-      </uni-list-item>
-    </uni-list>
-    <sl-vehicle-violation></sl-vehicle-violation>
-    <sl-vehicle-breakdown></sl-vehicle-breakdown>
-    <sl-vehicle-accident></sl-vehicle-accident>
+    <scroll-view class="scroll-view" scroll-y>
+      <view class="scroll-view-wrapper">
+        <uni-list class="base-info">
+          <uni-list-item
+            title="出车时间"
+            show-arrow
+            right-text="2022-05-04 13:00:00"
+          />
+          <uni-list-item show-arrow title="回车时间">
+            <template v-slot:footer>
+              <uni-datetime-picker v-model="dataTimePicker">
+                <view class="picker-value">{{
+                  dataTimePicker || '请选择'
+                }}</view>
+              </uni-datetime-picker>
+            </template>
+          </uni-list-item>
+        </uni-list>
+        <sl-vehicle-violation></sl-vehicle-violation>
+        <sl-vehicle-breakdown></sl-vehicle-breakdown>
+        <sl-vehicle-accident></sl-vehicle-accident>
+      </view>
+    </scroll-view>
+    <view class="toolbar">
+      <button class="button">提交登记</button>
+    </view>
   </view>
 </template>
 
