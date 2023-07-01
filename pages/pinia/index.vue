916e0b8b6720d8d0f37289ea56293190d97ff5e2
lotjol <botue@sina.com>
2023-07-01 16:26:33 +0800

    feat(pinia): add pinia playground

---
+++ pages/pinia/index.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+<template>
+  <view class="counter">
+    <button class="button" type="primary">-</button>
+    <input class="input" type="text" />
+    <button class="button" type="primary">+</button>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+  .counter {
+    display: flex;
+    padding: 100rpx;
+  }
+
+  .input {
+    flex: 1;
+    height: 96rpx;
+    text-align: center;
+    border: 2rpx solid #eee;
+    box-sizing: border-box;
+  }
+
+  .button {
+    width: 100rpx;
+    margin: 0;
+
+    &:first-child {
+      border-start-end-radius: 0;
+      border-end-end-radius: 0;
+    }
+    &:last-child {
+      border-start-start-radius: 0;
+      border-end-start-radius: 0;
+    }
+  }
+</style>
